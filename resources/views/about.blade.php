15bd8e25971455c9bc3a225455e8bab88869e7b4
adimasmudi <adimasmudi@gmail.com>
2022-06-09 17:41:54 +0700

    mempercantik

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,34 +1,46 @@
 <x-layout :active="$active">
     <!-- Content -->
-<div class="container">
-    <div class="row">
-        <h1 class="text-brand">About me</h1>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-brand">About me</h1>
+        </div>
+        <div class="row justify-content-center">
+            <img src="{{ asset('images/banner-foto.png') }}" alt="profil" class="image-profil">
+            <h2 class="text-brand text-center">Masmudi</h2>
+        </div>
+        <div class="row">
+            <h1 class="text-brand">Background</h1>
+            <p>
+                Perkenalkan nama saya Masmudi, saya berasal dari Bangkalan. Saya merupakan anak pertama, yap first child
+                dari 3 bersaudara bung.
+                Saya TK di Raudhatul Atfhal Al-Firadaus, di desa Moarah, kecamatan Klampis, Kabupaten Bangkalan. Setelah
+                itu, saya melanjutkan sekolah dasar di SDN Ra'as.
+                Setelah lulus SD saya lanjut ke SMPN 2 Klampis kemudian melanjutkan ke SMKN 1 Arosbaya jurusan Computer
+                and Network Engineering yang bahasa inggrisnya adalah TKJ.
+                Setelah lulus dari SMK tersebut sampailah saya di kampus UTM yang tidak tercinta ini. Saya kuliah di UTM
+                karena gagal dipilihan pertama dan jalur yang lain.
+                Saat itu, saya daftar SNMPTN dan meletakkan pilihan 2 2 nya di kampus top nasional yang ada di Surabaya
+                di 2 jurusan paling top pula. Selain itu, karena SMK saya juga dari SNMPN
+                di politeknik tetangga dari kampus tersebut, selain itu juga saya mau daftar ke STIS karena saya suka
+                statistik (setidaknya jika dibanding kalkulus) tapi tidak bisa karena sekolah kedinasan tersebut
+                tidak menerima anak lulusan SMK :) (menderita sekali ygy, cuma mau kuliah). Nah ternyata pendaftaran
+                SNMPTN dan SNMPN ditolak gess. Agak sedih sih, tapi gak sampe nangis karena sek ada jalan lain,
+                bismillah.
+                Akhirnya setelah itu daftarlah ke SBMPN yang harusnya ujian test ternyata malah seleksi raport lagi
+                (sama kek SNMPN), jadi ya gak lolos lagi dan emang feeling gak lolos sih karena seleksi raport biasanya
+                ngeliat nama
+                sekolah juga dan sekolah saya salah satu yang terbaik di Bangkalan soalnya (dari Belakang). Nah, karena
+                menderita kegagalan seperti itu akhirnya kena mental jadinya pilihan pertama tetap di taruh di pilihan
+                pertama SNMPTN, tapi
+                pilihan keduanya di Teknik Informatika UTM dan akhirnya saya pun mendekam disini :)
+            </p>
+        </div>
+        <div class="row">
+            <h1 class="text-brand">Suka Duka Praktikum PAW</h1>
+            <p>
+                Gak tau wkwk, keknya gak ada :)
+            </p>
+        </div>
     </div>
-    <div class="row justify-content-center">
-        <img src="{{asset('images/banner-foto.png')}}" alt="profil" class="image-profil">
-        <h2 class="text-brand text-center">Masmudi</h2>
-    </div>
-    <div class="row">
-        <h1 class="text-brand">Background</h1>
-        <p>
-            Perkenalkan nama saya Masmudi, saya berasal dari Bangkalan. Saya merupakan anak pertama, yap first child dari 3 bersaudara bung.
-            Saya TK di Raudhatul Atfhal Al-Firadaus, di desa Moarah, kecamatan Klampis, Kabupaten Bangkalan. Setelah itu, saya melanjutkan sekolah dasar di SDN Ra'as. 
-            Setelah lulus SD saya lanjut ke SMPN 2 Klampis kemudian melanjutkan ke SMKN 1 Arosbaya jurusan Computer and Network Engineering yang bahasa inggrisnya adalah TKJ.
-            Setelah lulus dari SMK tersebut sampailah saya di kampus UTM yang tidak tercinta ini. Saya kuliah di UTM karena gagal dipilihan pertama dan jalur yang lain.
-            Saat itu, saya daftar SNMPTN dan meletakkan pilihan 2 2 nya di kampus top nasional yang ada di Surabaya di 2 jurusan paling top pula. Selain itu, karena SMK saya juga dari SNMPN 
-            di politeknik tetangga dari kampus tersebut, selain itu juga saya mau daftar ke STIS karena saya suka statistik (setidaknya jika dibanding kalkulus) tapi tidak bisa karena sekolah kedinasan tersebut 
-            tidak menerima anak lulusan SMK :) (menderita sekali ygy, cuma mau kuliah). Nah ternyata pendaftaran SNMPTN dan SNMPN ditolak gess. Agak sedih sih, tapi gak sampe nangis karena sek ada jalan lain, bismillah.
-            Akhirnya setelah itu daftarlah ke SBMPN yang harusnya ujian test ternyata malah seleksi raport lagi (sama kek SNMPN), jadi ya gak lolos lagi dan emang feeling gak lolos sih karena seleksi raport biasanya ngeliat nama 
-            sekolah juga dan sekolah saya salah satu yang terbaik di Bangkalan soalnya (dari Belakang). Nah, karena menderita kegagalan seperti itu akhirnya kena mental jadinya pilihan pertama tetap di taruh di pilihan pertama SNMPTN, tapi
-            pilihan keduanya di Teknik Informatika UTM dan akhirnya saya pun mendekam disini :)
-        </p>
-    </div>
-    <div class="row">
-        <h1 class="text-brand">Suka Duka Praktikum PAW</h1>
-        <p>
-            Gak tau wkwk, keknya gak ada :)
-        </p>
-    </div>
-</div>
-<!-- End Content -->
+    <!-- End Content -->
 </x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,21 @@
 <x-layout :active="$active">
     <!-- Content -->
-<div class="container-md pt-4">
-    <h1 class="text-brand mb-4">Data Mahasiswa</h1>
-    <div class="row ps-2 pe-2">
-        
-        
-        @if (count($mahasiswa) > 0)
-        @foreach ($mahasiswa as $m)
-        <x-card :mahasiswa="$m" />
-    @endforeach
-        @else
-            <h6 style="margin-bottom:170px">Tidak ada data mahasiswa</h6>
-        @endif
-    
-        
-        
+    <div class="container-md pt-4">
+        <h1 class="text-brand mb-4">Data Mahasiswa</h1>
+        <div class="row ps-2 pe-2">
+
+
+            @if (count($mahasiswa) > 0)
+                @foreach ($mahasiswa as $m)
+                    <x-card :mahasiswa="$m" />
+                @endforeach
+            @else
+                <h6 style="margin-bottom:170px">Tidak ada data mahasiswa</h6>
+            @endif
+
+
+
+        </div>
     </div>
-</div>
-<!-- End Content -->
+    <!-- End Content -->
 </x-layout>
